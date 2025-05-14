286137f20d43d8d34f6abd3ad2f616f1fc866067
VPAbraham <vpa4565@gmail.com>
2025-05-14 12:23:17 -0600

    Define env variables and constants for RAWG API usage, add jest type dependencies, update gitignore for env files

---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+// Define base API URL and API key as constants
+export const API_BASE_URL =
+  import.meta.env.VITE_RAWG_BASE_URL || 'https://api.rawg.io/api';
+export const API_KEY = import.meta.env.VITE_RAWG_API_KEY || '';
