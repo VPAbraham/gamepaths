06e4a9cfa5986d97df43191f5fea7f811b0a67b9
VPAbraham <vpa4565@gmail.com>
2025-05-15 21:54:47 -0600

    Add React Router to App with home route to HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import './App.scss';
 
-function App() {
-  return <HomePage />;
-}
+const App = () => {
+  <Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  </Router>;
+};
 
 export default App;
